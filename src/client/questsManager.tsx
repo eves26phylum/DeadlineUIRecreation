399dc0509130af9cad4172a9a00760b3a3d7c235
eves <eves.26-phylum@icloud.com>
2026-04-12 23:44:25 +1000

    prevent infinite money bug

--- src/client/questsManager.tsx
+++ src/client/questsManager.tsx
@@ -36,6 +36,7 @@ export default function() {
         {/* Bob and Alice are inside a house. Bob is strong, and he can change the size of the house. Bob is really fat though, and Alice feels bad. It also is a bad look in photos, on why the couple are mismatched sizes in width. So, Alice decides to get fat herself. She can't change the size of the house, and if she gets too big, the walls of the house will crack again and the house explodes. Bob already cracked the house's walls once, so Alice wouldn't want to do it again. Alice uses useAbsoluteAxis, a service she found inside hooks. Alice uses this service as a guide on how big she can get. Alice tells Bob to measure himself, and Alice would size herself to this size that the experts at useAbsoluteAxis would tell her. */}
         <Basic Size={new UDim2(0, axis_content, 0, 0)} BackgroundColor3={Color3.fromRGB(255, 0, 0)} BackgroundTransparency={0}>
             <QuestTable tableOfQuests={questTableState} callback={(index: number) => {
+                if (questTableState[index].completed) return;
                 const cloneTableState = [...questTableState];
                 cloneTableState[index].completed = true;
                 const cloneMoney = {...money};
