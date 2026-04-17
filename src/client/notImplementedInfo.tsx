833ad5985a89c61764b8a7d5129e9da12c0211ad
eves <eves.26-phylum@icloud.com>
2026-04-17 16:11:45 +1000

    Updated loadout to include new format and added a not implemented UI template

---
+++ src/client/notImplementedInfo.tsx
@@ -0,0 +1,6 @@
+import React from "@rbxts/react";
+import { Basic, BasicProps } from "./easyobjects";
+
+export function NotImplemented({children, ...props}: {children: React.ReactNode} & BasicProps) {
+    return <Basic tags={["veryGenericBox", "hasOutline", "paddingStandard"]} textProps={{Tag: "textOnDemotivationCycle textBody"}} BackgroundTransparency={0} {...props}>{children}</Basic>
+}
