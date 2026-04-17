84619d12822e5b771508b1f046936da6aef80a39
eves <eves.26-phylum@icloud.com>
2026-04-17 18:55:34 +1000

    Added a license and changed not implemented info to include a default text case saying "Not implemented yet."

--- src/client/notImplementedInfo.tsx
+++ src/client/notImplementedInfo.tsx
@@ -2,5 +2,5 @@ import React from "@rbxts/react";
 import { Basic, BasicProps } from "./easyobjects";
 
 export function NotImplemented({children, ...props}: {children: React.ReactNode} & BasicProps) {
-    return <Basic tags={["veryGenericBox", "hasOutline", "paddingStandard"]} textProps={{Tag: "textOnDemotivationCycle textBody"}} BackgroundTransparency={0} {...props}>{children}</Basic>
+    return <Basic tags={["veryGenericBox", "hasOutline", "paddingStandard"]} textProps={{Tag: "textOnDemotivationCycle textBody"}} BackgroundTransparency={0} {...props}>{children || "Not implemented yet."}</Basic>
 }
